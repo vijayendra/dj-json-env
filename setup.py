45ef51cb14925dae9dde74972248f1c035ec3aa8
Vijayendra Bapte <vbapte@egnyte.com>
2016-05-13 10:22:31 -0700

    fixed script and released new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dj-json-env',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/vijayendra/dj-json-env',
     license='MIT',
     author='Vijayendra Bapte',
